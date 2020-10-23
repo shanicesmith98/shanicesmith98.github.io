6352c8ad7c1f7e05055ade1abccc3c52ab3bff40
Shanice Smith <shanicesmith.work@hotmail.com>
2020-10-23 12:33:23 -0400

    adding scroll to top

---
+++ js/main.js
@@ -0,0 +1,3 @@
+var offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
+offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+scrollDuration = 700;
