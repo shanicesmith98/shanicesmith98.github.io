2080744b3c5cdbcdc7a0ecc4403a38f4cf456da4
shanice s <shanices@shanices-MBP.lan>
2024-04-03 00:54:49 -0400

    behavior: create css file for dark mode styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-function turnOnDarkMode(){
-    var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+const body = document.querySelector('body');
+const toggle = document.getElementById('darkModeToggle');
+
+toggle.addEventListener("click", () => {
+    const bodyCheck = body.classList.contains('dark');
+    bodyCheck ? body.className = '' : body.className = "dark";
+})
